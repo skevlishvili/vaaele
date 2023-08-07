c373cfc65fc36e84e3571795bf9e93c7604c72be
Saba Kevlishvili <saba@theneo.io>
2023-08-08 02:33:04 +0400

    updated

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+var canvas = document.getElementById("myCanvas");
+
+class Confetti {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * -canvas.height;
+        this.speed = Math.random() * 4 + 1;
+        this.rotation = Math.PI * 2 * Math.random();
+        this.color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        this.size = Math.random() * 10 + 2;
+        this.xVel = Math.random() > 0.5 ? Math.random() : -Math.random();
+        this.yVel = Math.random() * 3 + 1;
+    }
+
+    update() {
+        this.y += this.yVel;
+        this.x += this.xVel;
+        this.rotation += 0.01;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+        ctx.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
+        ctx.restore();
+    }
+}
+
+
 window.onload = function() {
-    var canvas = document.getElementById("myCanvas");
 
     // Set canvas size to full viewport
     canvas.width = window.innerWidth;
@@ -12,17 +42,32 @@ window.onload = function() {
     var y = canvas.height / 2;
     var size = 30;
     var amplitude = 30;
-    var frequency = 0.05;
-    var speed = 0.05;
+    var frequency = 0.1; // increased frequency
+    var speed = 0.1; // increased speed
     var offsetX = 0;
     var step = 0;
 
-    ctx.font = size + "px Arial";
+    var confetti = [];
+    for (let i = 0; i < 200; i++) {
+        confetti.push(new Confetti());
+    }
+
+    ctx.font = size + "px 'Press Start 2P'";
+
+
     ctx.textAlign = "center";
-    ctx.fillStyle = "pink";
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < confetti.length; i++) {
+            confetti[i].update();
+            confetti[i].draw(ctx);
+            if (confetti[i].y > canvas.height) {
+                confetti[i] = new Confetti(); // Replace off-screen confetti
+            }
+        }
+
         for (var i = 0; i < step; i++) {
             var char = text[i];
 
@@ -30,11 +75,14 @@ window.onload = function() {
             var yPos = y + Math.sin((i + offsetX) * frequency) * amplitude;
             var xPos = x + (i - text.length / 2) * size;
 
+            // Create opacity transition for each character
+            ctx.fillStyle = "rgba(255, 105, 180, " + Math.min(step / i, 1) + ")"; // adjust RGBA values for desired color
+
             ctx.fillText(char, xPos, yPos);
         }
 
         offsetX += speed;
-        step = Math.min(step + 1, text.length);
+        step = Math.min(step + 0.5, text.length); // increased the step increment to make characters appear faster
 
         requestAnimationFrame(animate);
     }
